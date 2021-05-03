4102b27b8a62ebd548e9be035021d011da30c281
Jarryd Long <JarrydLong@users.noreply.github.com>
2021-05-03 15:49:57 +0200

    EDD SWS Module Added

---
+++ modules/class-swsales-module-edd.php
@@ -0,0 +1,518 @@
+<?php
+
+namespace Sitewide_Sales\modules;
+
+use Sitewide_Sales\classes;
+
+defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
+
+class SWSales_Module_EDD {
+
+	/**
+	 * Initial plugin setup
+	 *
+	 * @package sitewide-sale/modules
+	 */
+	public static function init() {
+		// Register sale type.
+		add_filter( 'swsales_sale_types', array( __CLASS__, 'register_sale_type' ) );
+
+		// Add fields to Edit Sitewide Sale page.
+		add_action( 'swsales_after_choose_sale_type', array( __CLASS__, 'add_choose_coupon' ) );
+
+		// Bail on additional functionality if WC is not active.
+		if ( ! class_exists( 'Easy_Digital_Downloads', false ) ) {
+			return;
+		}
+
+		// Enable saving of fields added above.
+		add_action( 'swsales_save_metaboxes', array( __CLASS__, 'save_metaboxes' ), 99, 2 );
+
+		// Enqueue JS for Edit Sitewide Sale page.
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+
+		// Generate coupons from editing sitewide sale.
+		add_action( 'wp_ajax_swsales_edd_create_coupon', array( __CLASS__, 'create_coupon_ajax' ) );
+
+		// Custom WC banner rules (hide at checkout).
+		add_filter( 'swsales_is_checkout_page', array( __CLASS__, 'is_checkout_page' ), 10, 2 );
+		
+		// For the swsales_coupon helper function
+		add_filter( 'swsales_coupon', array( __CLASS__, 'swsales_coupon' ), 10, 2 );
+
+		// Automatic coupon application.
+		add_filter( 'init', array( __CLASS__, 'automatic_coupon_application' ) );
+
+		// WC-specific reports.
+		add_filter( 'swsales_checkout_conversions_title', array( __CLASS__, 'checkout_conversions_title' ), 10, 2 );
+		add_filter( 'swsales_get_checkout_conversions', array( __CLASS__, 'checkout_conversions' ), 10, 2 );
+		add_filter( 'swsales_get_revenue', array( __CLASS__, 'sale_revenue' ), 10, 2 );
+		add_filter( 'swsales_daily_revenue_chart_data', array( __CLASS__, 'swsales_daily_revenue_chart_data' ), 10, 2 );
+		add_filter( 'swsales_daily_revenue_chart_currency_format', array( __CLASS__, 'swsales_daily_revenue_chart_currency_format' ), 10, 2 );
+		add_action( 'swsales_additional_reports', array( __CLASS__, 'additional_report' ) );
+	}
+
+	/**
+	 * Register WooCommerce module with SWSales
+	 *
+	 * @param  array $sale_types that are registered in SWSales.
+	 * @return array
+	 */
+	public static function register_sale_type( $sale_types ) {
+		$sale_types['edd'] = 'Easy Digital Downloads';
+		return $sale_types;
+	} // end register_sale_type()
+
+	/**
+	 * Adds option to choose coupon in Edit Sitewide Sale page.
+	 *
+	 * @param SWSales_Sitewide_Sale $cur_sale that is being edited.
+	 */
+	public static function add_choose_coupon( $cur_sale ) {
+		?>
+		<tr class='swsales-module-row swsales-module-row-edd'>
+			<?php if ( ! class_exists( 'Easy_Digital_Downloads', false ) ) { ?>
+				<th></th>
+				<td>
+					<div class="sitewide_sales_message sitewide_sales_error">
+						<p><?php echo esc_html( 'The Easy Digital Downloads plugin is not active.', 'sitewide-sales' ); ?></p>
+					</div>
+				</td>
+				<?php
+			} else {
+
+				//Compatible with EDD 2.9 and 3.0
+				$coupons = edd_get_discounts( array( 'active' ) );
+
+				$current_coupon = intval( $cur_sale->get_meta_value( 'swsales_edd_coupon_id', null ) );
+				?>
+					<th><label for="swsales_edd_coupon_id"><?php esc_html_e( 'Coupon', 'sitewide-sales' );?></label></th>
+					<td>
+						<select class="coupon_select swsales_option" id="swsales_edd_coupon_select" name="swsales_edd_coupon_id">
+							<option value="0"><?php esc_html_e( '- Choose One -', 'sitewide-sales' ); ?></option>
+							<?php
+							$coupon_found = false;
+							foreach ( $coupons as $coupon ) {
+								$selected_modifier = '';
+								if ( $coupon->ID === $current_coupon ) {
+									$selected_modifier = ' selected="selected"';
+									$coupon_found      = $coupon;
+								}
+								echo '<option value="' . esc_attr( $coupon->ID ) . '"' . $selected_modifier . '>' . esc_html( $coupon->post_title ) . '</option>';
+							}
+							?>
+						</select>
+						<?php
+						if ( false !== $coupon_found ) {
+							
+							$discount_object = new \EDD_Discount( $coupon_found->ID );
+							if ( null !== $discount_object->expiration && $cur_sale->get_end_date("Y-m-d") >= date('Y-m-d', intval( $discount_object->expiration ) ) ) {
+								echo "<p id='swsales_edd_coupon_expiry_error' class='sitewide_sales_message sitewide_sales_error'>" . __( "This coupon expires on or before the Sitewide Sale's end date.", 'sitewide-sales' ) . '</p>';
+							}
+						}
+						?>
+						<p>
+							<span id="swsales_edd_after_coupon_select">
+							<?php
+							if ( false !== $coupon_found ) {
+								$edit_coupon_url = admin_url( 'edit.php?post_type=download&page=edd-discounts&edd-action=edit_discount&discount='.$coupon_found->ID );
+							} else {
+								$edit_coupon_url = '#';
+							}
+
+							?>
+								<a target="_blank" class="button button-secondary" id="swsales_edd_edit_coupon" href="<?php echo esc_url( $edit_coupon_url ); ?>"><?php esc_html_e( 'edit discount code', 'sitewide-sales' ); ?></a>
+								<?php
+								esc_html_e( ' or ', 'sitewide-sales' );
+								?>
+							</span>
+							<button type="button" id="swsales_edd_create_coupon" class="button button-secondary"><?php esc_html_e( 'create a new discount code', 'sitewide-sales' ); ?></button>
+							<p class="description"><?php esc_html_e( 'Select the discount code that will be automatically applied for users when they visit your Landing Page.', 'sitewide-sales' ); ?></p>
+						</p>
+					</td>
+				<?php } ?>
+				</tr>
+		<?php
+	} // end add_choose_coupon()
+
+	/**
+	 * Saves WC module fields when saving Sitewide Sale.
+	 *
+	 * @param int     $post_id of the sitewide sale being edited.
+	 * @param WP_Post $post object of the sitewide sale being edited.
+	 */
+	public static function save_metaboxes( $post_id, $post ) {
+		if ( isset( $_POST['swsales_edd_coupon_id'] ) ) {
+			update_post_meta( $post_id, 'swsales_edd_coupon_id', intval( $_POST['swsales_edd_coupon_id'] ) );
+		}
+	}
+
+	/**
+	 * Enqueues /modules/js/swsales-module-wc-metaboxes.js
+	 */
+	public static function enqueue_scripts() {
+		global $wpdb, $typenow;
+		if ( 'sitewide_sale' === $typenow ) {
+			wp_register_script( 'swsales_module_edd_metaboxes', plugins_url( 'modules/js/swsales-module-edd-metaboxes.js', SWSALES_BASENAME ), array( 'jquery' ), '1.0.4' );
+			wp_enqueue_script( 'swsales_module_edd_metaboxes' );
+
+			wp_localize_script(
+				'swsales_module_edd_metaboxes',
+				'swsales_edd_metaboxes',
+				array(
+					'create_coupon_nonce' => wp_create_nonce( 'swsales_edd_create_coupon' ),
+					'admin_url'           => admin_url(),
+				)
+			);
+
+		}
+	} // end enqueue_scripts()
+
+	/**
+	 * AJAX callback to create a new coupon for your sale
+	 */
+	public static function create_coupon_ajax() {
+		global $wpdb;
+		check_ajax_referer( 'swsales_edd_create_coupon', 'nonce' );
+
+		$sitewide_sale_id = intval( $_REQUEST['swsales_edd_id'] );
+		if ( empty( $sitewide_sale_id ) ) {
+			echo json_encode(
+				array(
+					'status' => 'error',
+					'error'  => esc_html__(
+						'No sitewide sale ID given. Try doing it manually.',
+						'sitewide-sales'
+					),
+				)
+			);
+			exit;
+		}
+
+		/**
+		 * Create a coupon programatically
+		 */
+		global $wpdb;
+		while ( empty( $coupon_code ) ) {
+			$scramble = md5( AUTH_KEY . current_time( 'timestamp' ) . SECURE_AUTH_KEY );
+			$coupon_code = strtoupper( substr( $scramble, 0, 10 ) );			
+		}
+
+		if( !class_exists('EDD_Discount' ) ){
+			return;
+		}
+		
+		$amount        = '50'; // Amount.
+		$discount_type = 'percent'; // Type: flat, percent
+
+		$code = new \EDD_Discount();
+
+		$code->name = $coupon_code;
+		$code->code = $coupon_code;
+		$code->type = $discount_type;
+		$code->amount = $amount;
+
+		$code->save();
+
+		$r = array(
+			'status'      => 'success',
+			'coupon_id'   => $code->ID,
+			'coupon_code' => $coupon_code,
+		);
+		echo wp_json_encode( $r );
+		exit;
+	} // end create_discount_code_ajax()
+
+	/**
+	 * Returns whether the current page is the landing page
+	 * for the passed Sitewide Sale.
+	 *
+	 * @param boolean               $is_checkout_page current value from filter.
+	 * @param SWSales_Sitewide_Sale $sitewide_sale being checked.
+	 * @return boolean
+	 */
+	public static function is_checkout_page( $is_checkout_page, $sitewide_sale ) {		
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return $is_checkout_page;
+		}		
+		return is_page( edd_get_option( 'purchase_page', false ) ) ? true : $is_checkout_page;
+	}
+
+	/**
+	 * Get the coupon for a sitewide sale.
+	 * Callback for the swsales_coupon filter.
+	 */
+	public static function swsales_coupon( $coupon, $sitewide_sale ) {
+		if ( $sitewide_sale->get_sale_type() === 'edd' ) {
+			$coupon_object = new \EDD_Discount( $sitewide_sale->swsales_edd_coupon_id );			
+			if ( ! empty( $coupon_object ) ) {
+				$coupon = $coupon_object->code;
+			}
+		}
+
+		return $coupon;
+	}
+
+	public static function automatic_coupon_application() {
+		$active_sitewide_sale = classes\SWSales_Sitewide_Sale::get_active_sitewide_sale();
+		if ( null === $active_sitewide_sale || 'edd' !== $active_sitewide_sale->get_sale_type() || ! $active_sitewide_sale->should_apply_automatic_discount() ) {
+			return;
+		}
+
+		//Check if we're on the checkout page - Can't on the init hook though. 
+
+		// if ( !is_page( edd_get_option( 'purchase_page', false ) ) ) {
+		// 	return;
+		// }
+
+		$discount_code_id = intval( $active_sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null ) );
+
+		if ( 0 === $discount_code_id ) {
+			return;
+		}	
+
+		$code = new \EDD_Discount( $discount_code_id );
+ 		if ( !empty( $code ) ) {
+ 			//Set it in the $_REQUEST and EDD Session as the EDD init runs at priority 0
+ 			$_REQUEST['discount'] = $code->code;		 			
+ 			EDD()->session->set( 'preset_discount', $code->code );
+ 		} 		 		
+ 		
+	}
+	
+
+	/**
+	 * Set WC module checkout conversion title for Sitewide Sale report.
+	 *
+	 * @param string               $cur_title     set by filter.
+	 * @param SWSales_Sitewide_Sale $sitewide_sale to generate report for.
+	 * @return string
+	 */
+	public static function checkout_conversions_title( $cur_title, $sitewide_sale ) {
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return $cur_title;
+		}
+		$coupon_id = $sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null );
+		$coupon_code = new \EDD_Discount( $coupon_id );
+
+		if ( null === $coupon_id || empty( $coupon_code ) ) {
+			return $cur_title;
+		}
+
+		return sprintf(
+			__( 'Purchases using <a href="%s">%s</a>', 'sitewide-sales' ),
+			get_edit_post_link( $coupon_id ),
+			$coupon_code->name
+		);
+	}
+
+	/**
+	 * Set WC module checkout conversions for Sitewide Sale report.
+	 *
+	 * @param string               $cur_conversions set by filter.
+	 * @param SWSales_Sitewide_Sale $sitewide_sale to generate report for.
+	 * @return string
+	 */
+	public static function checkout_conversions( $cur_conversions, $sitewide_sale ) {
+		global $wpdb;
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return $cur_conversions;
+		}
+
+		$coupon_id   = $sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null );
+		$coupon_code = new \EDD_Discount( $coupon_id );
+
+		// $sale_start_date = $sitewide_sale->get_start_date('Y-m-d H:i:s');
+		// $sale_end_date = $sitewide_sale->get_end_date('Y-m-d H:i:s');
+		// $conversion_count = $wpdb->get_var( "
+		// 	SELECT COUNT(*)
+		// 	FROM {$wpdb->prefix}posts as p
+		// 	INNER JOIN {$wpdb->prefix}woocommerce_order_items as wcoi ON p.ID = wcoi.order_id
+		// 	WHERE p.post_type = 'shop_order'
+		// 	AND p.post_status IN ('wc-processing','wc-completed')
+		// 	AND p.post_date >= '{$sale_start_date}'
+		// 	AND p.post_date <= '{$sale_end_date}'
+		// 	AND upper(wcoi.order_item_name) = upper('{$coupon_code}')
+		// 	AND wcoi.order_item_type = 'coupon'
+		// " );
+
+		// return strval( $conversion_count );
+	}
+
+	/**
+	 * Set WC module total revenue for Sitewide Sale report.
+	 *
+	 * @param string               $cur_revenue set by filter.
+	 * @param SWSales_Sitewide_Sale $sitewide_sale to generate report for.
+	 * @return string
+	 */
+	public static function sale_revenue( $cur_revenue, $sitewide_sale ) {
+		global $wpdb;
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return $cur_revenue;
+		}
+
+		$sale_start_date = $sitewide_sale->get_start_date('Y-m-d H:i:s');
+		$sale_end_date = $sitewide_sale->get_end_date('Y-m-d H:i:s');
+		$coupon_id   = $sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null );
+		$coupon_code = new \EDD_Discount( $coupon_id );
+		// $sale_revenue = $wpdb->get_var( "
+		// 	SELECT DISTINCT SUM(pm.meta_value)
+		// 	FROM {$wpdb->prefix}posts as p
+		// 	INNER JOIN {$wpdb->prefix}woocommerce_order_items as wcoi ON p.ID = wcoi.order_id
+		// 	INNER JOIN {$wpdb->prefix}postmeta as pm ON p.ID = pm.post_id
+		// 	WHERE p.post_type = 'shop_order'
+		// 	AND p.post_status IN ('wc-processing','wc-completed')
+		// 	AND p.post_date >= '{$sale_start_date}'
+		// 	AND p.post_date <= '{$sale_end_date}'
+		// 	AND upper(wcoi.order_item_name) = upper('{$coupon_code}')
+		// 	AND wcoi.order_item_type = 'coupon'
+		// 	AND pm.meta_key = '_order_total'
+		// " );
+
+		// return wp_strip_all_tags( wc_price( $sale_revenue ) );
+	}
+
+	/**
+	 * Generate data for daily revenue chart.
+	 *
+	 * @param array                 $daily_revenue_chart_data to be shown in chart
+	 * @param SWSales_Sitewide_Sale $sitewide_sale being reported on.
+	 * @return bool
+	 */
+	public static function swsales_daily_revenue_chart_data( $daily_revenue_chart_data, $sitewide_sale ) {
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return $daily_revenue_chart_data;
+		}
+		global $wpdb;
+		$coupon_id   = $sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null );
+		$coupon_code = new \EDD_Discount( $coupon_id );
+		// $query_data = $wpdb->get_results(
+		// 	$wpdb->prepare(
+		// 		"
+		// 		SELECT DATE_FORMAT(p.post_date, '%s') as date, SUM(pm.meta_value) as value
+		// 			FROM {$wpdb->prefix}posts as p
+		// 		INNER JOIN {$wpdb->prefix}woocommerce_order_items as wcoi
+		// 			ON p.ID = wcoi.order_id
+		// 		INNER JOIN {$wpdb->prefix}postmeta as pm
+		// 			ON p.ID = pm.post_id
+		// 		WHERE p.post_type = 'shop_order'
+		// 			AND p.post_status IN ('wc-processing','wc-completed')
+		// 			AND p.post_date >= %s
+		// 			AND p.post_date <= %s
+		// 			AND upper(wcoi.order_item_name) = upper('%s')
+		// 			AND wcoi.order_item_type = 'coupon'
+		// 			AND pm.meta_key = '_order_total'
+		// 		GROUP BY date
+		// 		ORDER BY date
+		// 		",
+		// 		'%Y-%m-%d', // To prevent these from being seen as placeholders.
+		// 		$sitewide_sale->get_start_date( 'Y-m-d' ) . ' 00:00:00',
+		// 		$sitewide_sale->get_end_date( 'Y-m-d' ) . ' 23:59:59',
+		// 		$coupon_code
+		// 	)
+		// );
+		// foreach ( $query_data as $daily_revenue_obj ) {
+		// 	if ( array_key_exists( $daily_revenue_obj->date, $daily_revenue_chart_data ) ) {
+		// 		$daily_revenue_chart_data[$daily_revenue_obj->date] = floatval( $daily_revenue_obj->value );
+		// 	}
+		// }
+		// return $daily_revenue_chart_data;
+	}
+
+	public static function swsales_daily_revenue_chart_currency_format( $currency_format, $sitewide_sale ) {
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return;
+		}
+		return array(
+			'currency_symbol' => edd_get_currency(),
+			'decimals' => wc_get_price_decimals(),
+			'decimal_separator' => wc_get_price_decimal_separator(),
+			'thousands_separator' => wc_get_price_thousand_separator(),
+			'position' => strpos( get_option( 'woocommerce_currency_pos' ), 'right' ) !== false ? 'suffix' : 'prefix'
+		);
+	}
+
+	/**
+	 * Add additional PMPro module revenue report for Sitewide Sale.
+	 *
+	 * @param SWSales_Sitewide_Sale $sitewide_sale to generate report for.
+	 * @return string
+	 */
+	public static function additional_report( $sitewide_sale ) {
+		global $wpdb;
+		if ( 'edd' !== $sitewide_sale->get_sale_type() ) {
+			return;
+		}
+
+		$sale_start_date = $sitewide_sale->get_start_date('Y-m-d H:i:s');
+		$sale_end_date = $sitewide_sale->get_end_date('Y-m-d H:i:s');
+		$coupon_id   = $sitewide_sale->get_meta_value( 'swsales_edd_coupon_id', null );
+		$coupon_code = new \EDD_Discount( $coupon_id );
+
+		$total_rev = $wpdb->get_var( "
+			SELECT DISTINCT SUM(pm.meta_value)
+			FROM {$wpdb->prefix}posts as p
+			INNER JOIN {$wpdb->prefix}postmeta as pm ON p.ID = pm.post_id
+			WHERE p.post_type = 'shop_order'
+			AND p.post_status IN ('wc-processing','wc-completed')
+			AND p.post_date >= '{$sale_start_date}'
+			AND p.post_date <= '{$sale_end_date}'
+			AND pm.meta_key = '_order_total'
+		" );
+		$new_rev_with_code = $wpdb->get_var( "
+			SELECT DISTINCT SUM(pm.meta_value)
+			FROM {$wpdb->prefix}posts as p
+			INNER JOIN {$wpdb->prefix}woocommerce_order_items as wcoi ON p.ID = wcoi.order_id
+			INNER JOIN {$wpdb->prefix}postmeta as pm ON p.ID = pm.post_id
+			WHERE p.post_type = 'shop_order'
+			AND p.post_status IN ('wc-processing','wc-completed')
+			AND p.post_date >= '{$sale_start_date}'
+			AND p.post_date <= '{$sale_end_date}'
+			AND upper(wcoi.order_item_name) = upper('{$coupon_code}')
+			AND wcoi.order_item_type = 'coupon'
+			AND pm.meta_key = '_order_total'
+		" );
+		$new_rev_without_code = $total_rev - $new_rev_with_code;
+
+		?>
+		<div class="swsales_reports-box">
+			<h1 class="swsales_reports-box-title"><?php esc_html_e( 'Revenue Breakdown', 'sitewide-sales' ); ?></h1>
+			<p>
+				<?php
+				printf(
+					wp_kses_post( 'All orders from %s to %s.', 'sitewide-sales' ),
+					$sitewide_sale->get_start_date(),
+					$sitewide_sale->get_end_date()
+				);
+				?>
+			</p>
+			<hr />
+			<div class="swsales_reports-data swsales_reports-data-3col">
+				<div class="swsales_reports-data-section">
+					<h1><?php echo esc_attr( wp_strip_all_tags( wc_price( $new_rev_with_code ) ) ); ?></h1>
+					<p>
+						<?php esc_html_e( 'Sale Revenue', 'sitewide-sales' ); ?>
+						<br />
+						(<?php echo( esc_html( 0 == $total_rev ? 'NA' : round( ( $new_rev_with_code / $total_rev ) * 100, 2 ) ) ); ?>%)
+					</p>
+				</div>
+				<div class="swsales_reports-data-section">
+					<h1><?php echo esc_attr( wp_strip_all_tags( wc_price( $new_rev_without_code ) ) ); ?></h1>
+					<p>
+						<?php esc_html_e( 'Other New Revenue', 'sitewide-sales' ); ?>
+						<br />
+						(<?php echo( esc_html( 0 == $total_rev ? 'NA' : round( ( $new_rev_without_code / $total_rev ) * 100, 2 ) ) ); ?>%)
+					</p>
+				</div>
+				<div class="swsales_reports-data-section">
+					<h1><?php echo esc_attr( wp_strip_all_tags( wc_price( $total_rev ) ) ); ?></h1>
+					<p><?php esc_html_e( 'Total Revenue in Period', 'sitewide-sales' ); ?></p>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+
+}
+SWSales_Module_EDD::init();
---
+++ modules/js/swsales-module-edd-metaboxes.js
@@ -0,0 +1,58 @@
+jQuery( document ).ready(
+	function($) {
+
+		// multiselects
+		$( "#swsales_edd_coupon_select" ).selectWoo();
+
+		// toggling the discount code input layout
+		function swsales_edd_toggle_coupon() {
+			var coupon_id = $( '#swsales_edd_coupon_select' ).val();
+
+			if (coupon_id == 0) {
+				$( '#swsales_edd_after_coupon_select' ).hide();
+			} else {
+				$( '#swsales_edd_edit_coupon' ).attr( 'href', swsales_edd_metaboxes.admin_url + 'post.php?action=edit&post=' + coupon_id );
+				$( '#swsales_edd_after_coupon_select' ).show();
+			}
+		}
+		$( '#swsales_edd_coupon_select' ).change(
+			function(){
+				$( '#swsales_edd_coupon_expiry_error' ).hide();
+				swsales_edd_toggle_coupon();
+			}
+		);
+		swsales_edd_toggle_coupon();
+
+		// create new coupon AJAX
+		$( '#swsales_edd_create_coupon' ).click(
+			function() {
+				var data = {
+					'action': 'swsales_edd_create_coupon',
+					'swsales_edd_id': $( '#post_ID' ).val(),
+					'swsales_start': $( '#swsales_start_year' ).val() + '-'
+							 + $( '#swsales_start_month' ).val() + '-'
+							 + $( '#swsales_start_day' ).val(),
+					'swsales_end': $( '#swsales_end_year' ).val() + '-'
+							 + $( '#swsales_end_month' ).val() + '-'
+							 + $( '#swsales_end_day' ).val(),
+					'nonce': swsales_edd_metaboxes.create_coupon_nonce,
+				};
+				$.post(
+					ajaxurl,
+					data,
+					function(response) {
+						response = $.parseJSON( response );
+						if (response.status == 'error' ) {
+							alert( response.error );
+						} else {
+							// success
+							$( '#swsales_edd_coupon_select' ).append( '<option value="' + response.coupon_id + '">' + response.coupon_code + '</option>' );
+							$( '#swsales_edd_coupon_select' ).val( response.coupon_id );
+							swsales_edd_toggle_coupon();
+						}
+					}
+				);
+			}
+		);
+	}
+);
